336156c99b3fede9a143d75e6399c400ecb2280a
unicornsystum <142894471+unicornsystum@users.noreply.github.com>
2023-12-05 00:41:31 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from 'react'
+import './App.css'
+ 
 function App() {
+  const [value,setValue]=useState('');
+  
+  function handle(e){
+    setValue(value + e.target.innerHTML)
+  }
+  function equal(){
+    setValue(eval(value))
+  }
+  function AChandler(){
+    setValue('')
+  }
+  function dell(){
+     setValue(value.toString().slice(0,-1))
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='calculator'>
+      <input type='text' placeholder='0' value={value}/>
+      <div>
+        <button onClick={AChandler}>AC</button>
+        <button onClick={dell}>DEL</button>
+        <button className='operator'  onClick={handle}>%</button>
+        <button className='operator'  onClick={handle}>/</button>
+      </div>
+      <div>
+        <button onClick={handle}>7</button>
+        <button  onClick={handle}>8</button>
+        <button  onClick={handle}>9</button>
+        <button className='operator'  onClick={handle}>*</button>
+      </div>
+      <div>
+        <button  onClick={handle}>4</button>
+        <button  onClick={handle}>5</button>
+        <button  onClick={handle}>6</button>
+        <button className='operator'  onClick={handle}>-</button>
+      </div>
+      <div>
+        <button  onClick={handle}>1</button>
+        <button  onClick={handle}>2</button>
+        <button  onClick={handle}>3</button>
+        <button className='operator'  onClick={handle}>+</button>
+      </div>
+      <div>
+        <button  onClick={handle}>00</button>
+        <button  onClick={handle}>0</button>
+        <button  onClick={handle}>.</button>
+        <button className='equalbtn'  onClick={equal}>=</button>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
